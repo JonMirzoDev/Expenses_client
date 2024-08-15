bf00e928a95869f40ba3f3aef71dc5487329751a
JonMirzoDev <islomjon.swe@gmail.com>
2024-08-15 14:01:42 +0500

    auth context created

---
+++ src/app/context/AuthContext.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
+
+interface AuthContextType {
+  user: { username: string; email: string } | null
+  login: (userData: { username: string; email: string }) => void
+  logout: () => void
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<{ username: string; email: string } | null>(
+    null
+  )
+  const router = useRouter()
+
+  const login = (userData: { username: string; email: string }) => {
+    setUser(userData)
+    localStorage.setItem('user', JSON.stringify(userData))
+    router.push('/')
+  }
+
+  const logout = () => {
+    setUser(null)
+    localStorage.removeItem('user')
+    router.push('/login')
+  }
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user')
+    if (storedUser) {
+      setUser(JSON.parse(storedUser))
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  if (context == undefined) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
